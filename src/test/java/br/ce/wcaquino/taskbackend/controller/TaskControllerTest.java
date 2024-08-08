0987aa0a279a61ddb331093447eaa76bf60e8042
lhp97 <=>
2024-08-08 14:11:40 -0300

    Testes de applicacao

---
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -0,0 +1,78 @@
+package br.ce.wcaquino.taskbackend.controller;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
+import br.ce.wcaquino.taskbackend.utils.ValidationException;
+
+public class TaskControllerTest {
+	
+	@Mock
+	private TaskRepo taskRepo;
+	
+	@InjectMocks
+	private TaskController controller;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void naoDeveSalvarTaskSemDescricao() {
+		Task todo = new Task();
+		todo.setDueDate(LocalDate.now());
+		
+		try {
+			controller.save(todo);
+		} catch (ValidationException e) {
+			Assert.assertEquals("Fill the task description", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void naoDeveSalvarTaskSemData() {
+		Task todo = new Task();
+		todo.setTask("Teste");
+		
+		try {
+			controller.save(todo);
+		} catch (ValidationException e) {
+			Assert.assertEquals("Fill the due date", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void naoDeveSalvarTaskComDataPassada() {
+		Task todo = new Task();
+		todo.setDueDate(LocalDate.of(2010, 01, 01));
+		todo.setTask("Teste");
+		
+		try {
+			controller.save(todo);
+		} catch (ValidationException e) {
+			Assert.assertEquals("Due date must not be in past", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void deveSalvarTaskComSucesso() throws ValidationException {
+		Task todo = new Task();
+		todo.setDueDate(LocalDate.now());
+		todo.setTask("Teste");
+		
+		controller.save(todo);
+		
+		Mockito.verify(taskRepo).save(todo);
+	}
+
+}
